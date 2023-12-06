31bacee6ac4f28abe1caff0b89d45dfaeaa62a27
SeonUngMoon <138311908+Qq00011011@users.noreply.github.com>
2023-12-06 23:15:21 +0900

    Add files via upload

---
+++ 5주차/SeonungMoon/boj_15650_문선웅.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void generateSequences(int N, int M, int index, vector<int>& sequence) {
+    // 기저 조건: 수열의 길이가 M이면 출력하고 종료
+    if (sequence.size() == M) {
+        for (int num : sequence) {
+            cout << num << " ";
+        }
+        cout << '\n';
+        return;
+    }
+
+    // 현재 선택한 숫자 이후의 숫자만을 고르도록 반복
+    for (int i = index; i <= N; ++i) {
+        sequence.push_back(i);  // 현재 숫자 추가
+        generateSequences(N, M, i + 1, sequence);  // 재귀 호출
+        sequence.pop_back();    // 현재 숫자 제거
+    }
+}
+
+int main() {
+    // 입력 받기
+    int N, M;
+    cin >> N >> M;
+
+    // 수열을 저장할 벡터 초기화
+    vector<int> sequence;
+
+    // 재귀 함수 호출
+    generateSequences(N, M, 1, sequence);
+
+    return 0;
+}
