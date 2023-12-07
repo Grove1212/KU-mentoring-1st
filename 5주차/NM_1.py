a4a973f2c4c53f01133aee3bb767961b9e14d47f
unknown <zeroplam5@gmail.com>
2023-12-07 20:35:35 +0900

    [complete]_NM문제1

---
+++ 5주차/NM_1.py
@@ -0,0 +1,20 @@
+N, M = map(int,input().split())
+
+A = []
+for i in range(1,N+1):
+    A.append(i)
+
+B = []
+
+def permutation():
+        if len(B) == M:
+            for i in B:
+                print(i,end=' ')
+            print()
+        else:
+            for i in A:
+                if i not in B:
+                    B.append(i)
+                    permutation()
+                    B.pop()
+permutation()
